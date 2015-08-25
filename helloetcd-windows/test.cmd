c78886d20a716e1b4229feb67485c893b7680d46
Robert Labrie <robert.labrie@gmail.com>
2015-08-24 18:33:45 -0700

    change container name

--- helloetcd-windows/test.cmd
+++ helloetcd-windows/test.cmd
@@ -1,6 +1,6 @@
 @echo off
-docker build -t hellosite .
-docker run -it --name hellosite -p 80:80 -e ETCD_PROXY=on -e ETCD_LISTEN_CLIENT_URLS=http://127.0.0.1:2379 -e "ETCD_INITIAL_CLUSTER=win2016=http://10.0.1.10:2380" hellosite cmd
+docker build -t helloetcd .
+docker run -it --name helloetcd -p 80:80 -e ETCD_PROXY=on -e ETCD_LISTEN_CLIENT_URLS=http://127.0.0.1:2379 -e "ETCD_INITIAL_CLUSTER=win2016=http://10.0.1.10:2380" helloetcd cmd
 pause
-docker kill hellosite
-docker rm hellosite
+docker kill helloetcd
+docker rm helloetcd
