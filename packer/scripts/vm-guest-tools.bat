3e0b418b68b7aa1bd3277af9df00024d5ff63d5a
Robert Labrie <robert.labrie@gmail.com>
2015-08-28 20:44:08 -0400

    ewww 7zip ... this needs a refactor

--- packer/scripts/vm-guest-tools.bat
+++ packer/scripts/vm-guest-tools.bat
@@ -1,3 +1,8 @@
+if not exist "C:\Windows\Temp\7z920-x64.msi" (
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z920-x64.msi', 'C:\Windows\Temp\7z920-x64.msi')" <NUL
+)
+msiexec /qb /i C:\Windows\Temp\7z920-x64.msi
+
 if "%PACKER_BUILDER_TYPE%" equ "vmware-iso" goto :vmware
 if "%PACKER_BUILDER_TYPE%" equ "virtualbox-iso" goto :virtualbox
 if "%PACKER_BUILDER_TYPE%" equ "parallels-iso" goto :parallels
