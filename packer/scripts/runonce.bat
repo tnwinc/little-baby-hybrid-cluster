8abed8b8f0fa3acc141956eba8812572442f1555
Robert Labrie <robert.labrie@gmail.com>
2015-08-29 21:03:20 -0400

    massive cleanup

--- packer/scripts/runonce.bat
+++ packer/scripts/runonce.bat
@@ -1,2 +1,7 @@
 A:
-for /f %%f in ('dir /b *.cmd') do call %%f
+for /f %%f in ('dir /b *.cmd') do (
+	echo starting %%f c:\packer.log
+	call %%f
+	echo finished %%f c:\packer.log
+)
+echo done > c:\status.txt
---
+++ packer/setup.cmd
@@ -0,0 +1,5 @@
+choco cup -y chocolatey
+choco install -y packer
+choco install -y packer-windows-plugins
+choco install -y packer-post-processor-vagrant-vmware-ovf
+choco install -y vagrant
