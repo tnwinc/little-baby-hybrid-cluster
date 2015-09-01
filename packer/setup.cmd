ff2ecc373879c4315ca6c52e0362d47dd61959ac
Robert Labrie <robert.labrie@gmail.com>
2015-09-01 15:17:12 -0400

    vbox is back

--- packer/setup.cmd
+++ packer/setup.cmd
@@ -3,3 +3,5 @@ choco install -y packer
 choco install -y packer-windows-plugins
 choco install -y packer-post-processor-vagrant-vmware-ovf
 choco install -y vagrant
+go get github.com/iancmcc/packer-post-processor-ovftool
+go install github.com/iancmcc/packer-post-processor-ovftool
--- packer/test.bat
+++ packer/test.bat
@@ -1,2 +1,3 @@
 rmdir /s /q output-vmware-iso
+rmdir /s /q output-virtualbox-iso
 packer build windows_2016_tp3.json
